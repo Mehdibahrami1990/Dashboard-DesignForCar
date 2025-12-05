65983bd09e1c1258dae2dc337162dd866c7cf0e8
mehdi <tbmgroup20@gmail.com>
2025-12-06 00:46:00 +0330

    feat: Create Fuel Section

--- src/components/MainHome.tsx
+++ src/components/MainHome.tsx
@@ -5,11 +5,11 @@ import Container from "./Container";
 const MainHome = () => {
   return (
     <>
-      <Container>
+      <>
         <div className="px-4 sm:px-8 lg:px-12 py-10 flex flex-col gap-8 lg:flex-row">
           <Dashboard />
         </div>
-      </Container>
+      </>
     </>
   );
 };
